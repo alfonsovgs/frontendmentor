9b929e8f2c78baa571935486bce287ea16c044a1
Alfonso <vc.alfonso@hotmail.com>
2020-06-30 13:07:33 -0500

    four-card-feature-section Tailwind completed

---
+++ challenges/four-card-feature-section/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  theme: {
+    screens: {
+      sm: "375px",
+      md: '1440px',
+    },
+    fontFamily: {
+      body: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'red-primary': 'hsl(0, 78%, 62%)',
+        'cyan-primary': 'hsl(180, 62%, 55%)',
+        'orange-primary': 'hsl(34, 97%, 64%)',
+        'blue-primary': 'hsl(212, 86%, 64%)',
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '136': '32rem',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
